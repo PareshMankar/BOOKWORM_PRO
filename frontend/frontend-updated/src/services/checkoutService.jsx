dc67dae36b2262d3fe4334ee2ae7262166cc5e31
Punammotewar <punammotewar@gmail.com>
2026-02-11 15:04:10 +0530

    df

--- frontend/frontend-updated/src/services/checkoutService.jsx
+++
@@ -1,6 +0,0 @@
-import api from "../api/axios";
-
-// Record a new transaction
-export const checkoutAPI = (transactionData) => {
-    return api.post("/checkout", transactionData);
-};
--- frontend/frontend-updated/src/services/shelfService.jsx
+++
@@ -1,7 +0,0 @@
-import api from "../api/axios";
-
-
-// Get the user's shelf
-export const getShelfAPI = (userId) => {
-    return api.get(`/shelf/${userId}`);
-};
